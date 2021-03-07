297e0d1dc8452a9682c770facfa484b1f2740531
b-amitsingh <8141amitsingh@gmail.com>
2021-03-08 04:35:51 +0530

    Navigation added

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavigationItems from '../NavigationItems/NavigationItems'
+
+import Logo from '../../Logo/Logo'
+import classes from './Toolbar.module.css'
+
+const toolbar = () => (
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <Logo />
+        <nav>
+            <NavigationItems />
+        </nav>
+    </header>
+)
+
+export default toolbar;
